d6d5393f1dbc9a15eecd983bdf0ddaec7597cc95
chenenkou <1187232704@qq.com>
2016-05-24 23:07:27 +0800

    添加采集类

---
+++ Frame/Lib/SimpleHtmlDom.php
@@ -0,0 +1,9 @@
+<?php
+// https://github.com/samacs/simple_html_dom
+class SimpleHtmlDom {
+
+    public function __construct()
+    {
+        require_once(LIB_PATH . "/Util/SimpleHtmlDom/simple_html_dom.php");
+    }
+}
