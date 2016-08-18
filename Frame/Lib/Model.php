ea0a864a6b5ff103d63a6f4b46fdb0fcff788675
S76 <S76hckj@ooopic.com>
2016-08-18 14:36:18 +0800

    优化Model

--- Frame/Lib/Model.php
+++ Frame/Lib/Model.php
@@ -1,30 +1,32 @@
 <?php
+
 /**
  * 基础数据模型
  * Date: 15-7-28
  * Time: 下午4:24
  */
-
-class Model {
+class Model
+{
     // 数据库连接
     protected $_db;
     // 数据库配置 - 子类可以修改该属性切换数据库
     protected $db_config = 'DB_CONFIG0';
-    // 模型对应表名
+    // 模型对应表名 - 子类可以修改该属性标记正确的表名
     protected $table_name = null;
 
     /**
      * 初始化
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->connectDb();
     }
 
     /**
      * 连接数据库
-     * @param $dbConfig
      */
-    public function connectDb() {
+    public function connectDb()
+    {
         $this->_db = $this->getDbConnection();
         $this->table_name = $this->tableName();
     }
@@ -34,7 +36,8 @@ class Model {
      * 子类可以覆盖该方法切换数据库
      * @return Model
      */
-    public function getDbConnection() {
+    public function getDbConnection()
+    {
         return D($this->db_config);
     }
 
@@ -43,18 +46,20 @@ class Model {
      * 子类应该覆盖该方法表明对应数据表
      * @return null
      */
-    public function tableName() {
-        return $this->_db->getTablePrefix() . hump2underline(get_class($this));
+    public function tableName()
+    {
+        return $this->table_name ? $this->table_name : $this->_db->getTablePrefix() . hump2underline(get_class($this));
     }
 
     /**
      * 执行查询 主要针对 SELECT, SHOW 等指令
      * 返回数据集
      * @access public
-     * @param string $str  sql指令
+     * @param string $str sql指令
      * @return mixed
      */
-    public function query($sql) {
+    public function query($sql)
+    {
         return $this->_db->query($sql);
     }
 
@@ -62,10 +67,11 @@ class Model {
      * 执行查询 主要针对 SELECT等指令
      * 返回一条字段
      * @access public
-     * @param string $str  sql指令
+     * @param string $sql sql指令
      * @return mixed
      */
-    public function find($sql) {
+    public function find($sql)
+    {
         return $this->_db->find($sql);
     }
 
@@ -73,10 +79,11 @@ class Model {
      * 执行查询 主要针对 SELECT等指令
      * 返回一条数目
      * @access public
-     * @param string $str  sql指令
+     * @param string $sql sql指令
      * @return mixed
      */
-    public function count($sql) {
+    public function count($sql)
+    {
         return $this->_db->count($sql);
     }
 
@@ -84,20 +91,22 @@ class Model {
      * 获取一个表中的字段名
      * 返回字段名数组
      * @access public
-     * @param string $table_name  表名
+     * @param string $table_name 表名
      * @return mixed field为字段名 pri为主键
      */
-    public function getColumns($table_name) {
+    public function getColumns($table_name)
+    {
         return $this->_db->getColumns($table_name);
     }
 
     /**
      * 执行语句 针对 INSERT, UPDATE 以及DELETE
      * @access public
-     * @param string $str  sql指令
+     * @param string $sql sql指令
      * @return integer
      */
-    public function execute($sql) {
+    public function execute($sql)
+    {
         $result = $this->_db->execute($sql);
         if (!(stripos($sql, 'INSERT') === false)) {
             if ($result > 0) {
@@ -112,25 +121,28 @@ class Model {
      * @access public
      * @return void
      */
-    public function startTrans() {
+    public function startTrans()
+    {
         return $this->_db->startTrans();
     }
 
     /**
      * 用于非自动提交状态下面的查询提交
      * @access public
-     * @return boolen
+     * @return bool
      */
-    public function commit() {
+    public function commit()
+    {
         return $this->_db->commit();
     }
 
     /**
      * 事务回滚
      * @access public
-     * @return boolen
+     * @return bool
      */
-    public function rollback() {
+    public function rollback()
+    {
         return $this->_db->rollback();
     }
 
@@ -139,7 +151,8 @@ class Model {
      * @access public
      * @return string
      */
-    public function getLastSql() {
+    public function getLastSql()
+    {
         return $this->_db->getLastSql();
     }
 
@@ -148,17 +161,19 @@ class Model {
      * @access public
      * @return string
      */
-    public function getLastInsID(){
+    public function getLastInsID()
+    {
         return $this->_db->getLastInsID();
     }
 
     /**
-     * 保存数据
-     * @param $data 需要保存的数据
+     * 新增数据前的处理
+     * @param array $data 需要处理的数据
      * @param string $table 表名
-     * @return int
+     * @return array
      */
-    public function insert($data, $table='') {
+    private function _addData($data, $table = '')
+    {
         // 判断是单条数据还是多条数据
         if (is_array(end($data))) {
             list($fields, $values) = doubleArr2InsertSql($data);
@@ -167,10 +182,37 @@ class Model {
             $values = "({$values})";
         }
         // 处理需要插入的表名
-        if ( empty($table) )
+        if (empty($table))
             $table = $this->tableName();
 
+        return array($table, $fields, $values);
+    }
+
+    /**
+     * 保存数据
+     * @param array $data 需要保存的数据
+     * @param string $table 表名
+     * @return int
+     */
+    public function insert($data, $table = '')
+    {
+        list($table, $fields, $values) = $this->_addData($data, $table);
+
         $sql = "INSERT INTO {$table} ({$fields}) VALUES {$values}";
+        return $this->execute($sql);
+    }
+
+    /**
+     * 替换数据
+     * @param array $data 需要保存的数据
+     * @param string $table 表名
+     * @return int
+     */
+    public function replace($data, $table = '')
+    {
+        list($table, $fields, $values) = $this->_addData($data, $table);
+
+        $sql = "REPLACE INTO {$table} ({$fields}) VALUES {$values}";
         return $this->execute($sql);
     }
 }
