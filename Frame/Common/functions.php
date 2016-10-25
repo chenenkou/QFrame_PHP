9b67595703ec3cc480ddbc6e0b3873b8094e4dd9
S76 <S76hckj@ooopic.com>
2016-10-25 10:01:14 +0800

    add read_file_down

--- Frame/Common/functions.php
+++ Frame/Common/functions.php
@@ -378,4 +378,26 @@ function array_remove_key($arr, $key)
     return $arr;
 }
 
+/**
+ * 读取本地文件提供下载
+ * @param $file string 本地文件名
+ */
+function read_file_down($file)
+{
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.basename($file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        ob_clean();
+        flush();
+        readfile($file);
+        exit;
+    }
+}
+
 ?>
